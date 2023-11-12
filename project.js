56d704eb29a88b0f0bb7b1064dc2d3781be751ea
Manya Gogia <gogiamanya@gmail.com>
2023-11-12 07:40:55 -0600

    carrers and newcars update

--- project.js
+++ project.js
@@ -1,16 +1,19 @@
 function myMove() {
-    let id = null;
-    const elem = document.getElementById("mover");
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 100);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-        elem.style.left = 0 + 'px';
-      } else {
-        pos+=10;
-        elem.style.left = pos + 'px';
-      }
+  let id = null;
+  const elem = document.getElementById("mover");
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 100);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+      elem.style.left = 0 + 'px';
+    } else {
+      pos+=10;
+      elem.style.left = pos + 'px';
     }
-  } 
+  }
+} 
+function applyNow() {
+  alert("Thank you for applying! We will get in touch with you soon.");
+}
